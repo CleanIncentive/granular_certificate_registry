131ae128275c002b5b66114595ebfee4a9cf50ce
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-01-02 16:14:32 +0000

    Extend auth to test cases

--- gc_registry/tests/measurement/test_measurement_routes.py
+++ gc_registry/tests/measurement/test_measurement_routes.py
@@ -1,28 +1,31 @@
 import json
 
 import pytest
+from fastapi.testclient import TestClient
+
+from gc_registry.device.models import Device
 
 
 @pytest.fixture
-def valid_measurement_json():
+def valid_measurement_json(fake_db_solar_device: Device, fake_db_wind_device: Device):
     return json.dumps(
         [
             {
-                "device_id": 1,
+                "device_id": fake_db_solar_device.id,
                 "interval_usage": 10,
                 "interval_start_datetime": "2024-11-18T10:00:00",
                 "interval_end_datetime": "2024-11-18T11:00:00",
                 "gross_net_indicator": "NET",
             },
             {
-                "device_id": 1,
+                "device_id": fake_db_solar_device.id,
                 "interval_usage": 15,
                 "interval_start_datetime": "2024-11-18T11:00:00",
                 "interval_end_datetime": "2024-11-18T12:00:00",
                 "gross_net_indicator": "NET",
             },
             {
-                "device_id": 2,
+                "device_id": fake_db_wind_device.id,
                 "interval_usage": 20,
                 "interval_start_datetime": "2024-11-18T10:00:00",
                 "interval_end_datetime": "2024-11-18T11:00:00",
@@ -33,20 +36,27 @@ def valid_measurement_json():
 
 
 def test_submit_readings_success(
-    api_client,
-    valid_measurement_json,
+    api_client: TestClient,
+    token: str,
+    valid_measurement_json: str,
+    fake_db_solar_device: Device,
+    fake_db_wind_device: Device,
 ):
     """Test successful submission of readings."""
 
     response = api_client.post(
-        "measurement/submit_readings/",
+        "measurement/submit_readings",
         params={"measurement_json": valid_measurement_json},
+        headers={"Authorization": f"Bearer {token}"},
     )
 
     assert response.status_code == 200
 
     response_data = response.json()
     assert response_data["message"] == "Readings submitted successfully."
-    assert response_data["total_usage_per_device"] == {"1": 25, "2": 20}
+    assert response_data["total_usage_per_device"] == {
+        str(fake_db_solar_device.id): 25,
+        str(fake_db_wind_device.id): 20,
+    }
     assert response_data["first_reading_datetime"] == "2024-11-18T10:00:00"
     assert response_data["last_reading_datetime"] == "2024-11-18T12:00:00"
