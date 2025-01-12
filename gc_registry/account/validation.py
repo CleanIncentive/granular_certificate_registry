404dcff03605dea5b84361e47c39664d9e050302
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-01-12 18:56:11 +0000

    Modify whitelist service to update link model instead

--- gc_registry/account/validation.py
+++ gc_registry/account/validation.py
@@ -1,8 +1,9 @@
+from esdbclient import EventStoreDBClient
 from fastapi import HTTPException
 from sqlalchemy import func
 from sqlmodel import Session, select
 
-from gc_registry.account.models import Account
+from gc_registry.account.models import Account, AccountWhitelistLink
 from gc_registry.account.schemas import AccountBase, AccountWhitelist
 from gc_registry.user.models import User
 
@@ -35,22 +36,28 @@ def validate_account(account: Account | AccountBase, read_session: Session):
             )
 
 
-def validate_account_whitelist_update(
-    account: Account, account_whitelist_update: AccountWhitelist, read_session: Session
+def validate_and_apply_account_whitelist_update(
+    account: Account,
+    account_whitelist_update: AccountWhitelist,
+    write_session: Session,
+    read_session: Session,
+    esdb_client: EventStoreDBClient,
 ):
-    """Ensure that the account whitelist update is valid by checking that the accounts in question exist.
+    """Ensure that the account whitelist update is valid by checking that the accounts in question exist,
+    and writes to the whitelist link table if the update is valid.
 
     Args:
         account (Account): The account to be updated.
         account_whitelist_update (AccountWhitelist): The whitelist update to be applied.
         read_session (Session): The database session to read from.
-
-    Returns:
-        modified_whitelist: The modified whitelist to be applied to the account.
+        write_session (Session): The database session to write to.
+        esdb_client (EventStoreDBClient): The EventStoreDB client to use for event sourcing.
     """
-    existing_whitelist = (
-        [] if account.account_whitelist is None else account.account_whitelist
-    )
+    existing_whitelist = read_session.exec(
+        select(AccountWhitelistLink.source_account_id).where(
+            AccountWhitelistLink.target_account_id == account.id
+        )
+    ).all()
 
     if account_whitelist_update.add_to_whitelist is not None:
         for account_id_to_add in account_whitelist_update.add_to_whitelist:
@@ -64,14 +71,45 @@ def validate_account_whitelist_update(
                     status_code=404,
                     detail=f"Account ID to add not found: {account_id_to_add}",
                 )
-        modified_whitelist = list(
-            set(existing_whitelist + account_whitelist_update.add_to_whitelist)  # type: ignore
+            if (existing_whitelist is not None) and (
+                account_id_to_add in existing_whitelist
+            ):
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Account ID {account_id_to_add} is already in the whitelist.",
+                )
+        AccountWhitelistLink.create(
+            [
+                {
+                    "target_account_id": account.id,
+                    "source_account_id": account_id_to_add,
+                }
+                for account_id_to_add in account_whitelist_update.add_to_whitelist
+            ],
+            write_session=write_session,
+            read_session=read_session,
+            esdb_client=esdb_client,
         )
 
     if account_whitelist_update.remove_from_whitelist is not None:
-        modified_whitelist = list(
-            set(existing_whitelist)
-            - set(account_whitelist_update.remove_from_whitelist)  # type: ignore
-        )
-
-    return modified_whitelist
+        for account_id_to_remove in account_whitelist_update.remove_from_whitelist:
+            if not Account.exists(account_id_to_remove, read_session):
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Account ID to remove not found: {account_id_to_remove}",
+                )
+            account_whitelist_link_to_remove = read_session.exec(
+                select(AccountWhitelistLink).where(
+                    AccountWhitelistLink.target_account_id == account.id,
+                    AccountWhitelistLink.source_account_id == account_id_to_remove,
+                    AccountWhitelistLink.is_deleted == False,  # noqa: E712
+                )
+            ).first()
+            account_whitelist_link_to_remove = AccountWhitelistLink(
+                **account_whitelist_link_to_remove
+            )
+            account_whitelist_link_to_remove.delete(
+                write_session=write_session,
+                read_session=read_session,
+                esdb_client=esdb_client,
+            )
