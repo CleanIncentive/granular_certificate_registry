2d086dbe30ab2f38daa18364b097dbac7c59e5a8
Clem <clem@futureenergy.associates>
2025-01-21 22:40:21 +0000

    Linting

--- gc_registry/authentication/routes.py
+++ gc_registry/authentication/routes.py
@@ -5,9 +5,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
+from gc_registry.authentication import services
 from gc_registry.authentication.models import TokenRecords
 from gc_registry.authentication.schemas import Token
-from gc_registry.authentication import services
 from gc_registry.core.database import db, events
 from gc_registry.settings import settings as st
 
@@ -23,14 +23,16 @@ async def login_for_access_token(
     read_session: Session = Depends(db.get_read_session),
     esdb_client: EventStoreDBClient = Depends(events.get_esdb_client),
 ):
-    user = services.authenticate_user(form_data.username, form_data.password, read_session)
+    user = services.authenticate_user(
+        form_data.username, form_data.password, read_session
+    )
     access_token_expires = timedelta(minutes=st.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = services.create_access_token(
         data={"sub": user.name}, expires_delta=access_token_expires
     )
 
     if user.id is None:
-        raise HTTPException(status_code=404,detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
     # save username and token to database to match future requests
     token_record = TokenRecords(
