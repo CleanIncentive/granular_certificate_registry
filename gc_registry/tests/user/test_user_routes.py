1b6e48155e8a8c15307b98d6c93adcea3a246956
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-01-24 11:16:03 +0000

    Add incorrect token tets case

--- gc_registry/tests/user/test_user_routes.py
+++ gc_registry/tests/user/test_user_routes.py
@@ -7,6 +7,14 @@ class TestUserRoutes:
     def test_read_current_user(
         self, api_client: TestClient, fake_db_user: User, token: str
     ):
+        # Test that the current user is returned
         res = api_client.get("/user/me", headers={"Authorization": f"Bearer {token}"})
         assert res.status_code == 200
         assert res.json()["name"] == fake_db_user.name
+
+        # Test that the current user is not returned if the token is invalid
+        res = api_client.get(
+            "/user/me", headers={"Authorization": "Bearer invalid_token"}
+        )
+        assert res.status_code == 401
+        assert res.json()["detail"] == "Could not validate credentials"
