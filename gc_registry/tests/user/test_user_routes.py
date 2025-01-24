f62012b27566c37e320aeb2ad863ded1afb2fd8c
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-01-24 12:07:21 +0000

    Add endpoint for get current user's accounts

--- gc_registry/tests/user/test_user_routes.py
+++ gc_registry/tests/user/test_user_routes.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 
+from gc_registry.account.models import Account
+from gc_registry.account.schemas import AccountRead
 from gc_registry.user.models import User
 
 
@@ -18,3 +20,19 @@ class TestUserRoutes:
         )
         assert res.status_code == 401
         assert res.json()["detail"] == "Could not validate credentials"
+
+    def test_read_current_user_accounts(
+        self,
+        api_client: TestClient,
+        fake_db_account: Account,
+        fake_db_account_2: Account,
+        token: str,
+    ):
+        res = api_client.get(
+            "/user/me/accounts", headers={"Authorization": f"Bearer {token}"}
+        )
+        assert res.status_code == 200
+        assert [AccountRead.model_validate(a) for a in res.json()] == [
+            AccountRead.model_validate(fake_db_account.model_dump()),
+            AccountRead.model_validate(fake_db_account_2.model_dump()),
+        ]
