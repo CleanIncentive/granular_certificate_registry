b42b3b7ea58b254b9391914279c223365815039a
truongdb <truongdb@devempower.com.vn>
2025-01-25 18:27:48 +0000

    Add getAccountDetails Thunk and remove required for is_storage checkbox

--- frontend/components/device/DeviceRegisterForm.js
+++ frontend/components/device/DeviceRegisterForm.js
@@ -6,6 +6,7 @@ import { useDispatch } from "react-redux";
 import Cookies from "js-cookie";
 
 import { createDevice } from "../../store/device/deviceThunk";
+import { getAccountDetails } from "../../store/account/accountThunk";
 
 const { Option } = Select;
 
@@ -29,7 +30,7 @@ const DeviceRegisterDialog = forwardRef((props, ref) => {
   const handleOk = async () => {
     try {
       const values = await form.validateFields();
-      values.location = `${values.location.latitude}, ${values.location.longitude}`
+      values.location = `${values.location.latitude}, ${values.location.longitude}`;
       console.log("Device registration values:", values);
       const resonse = await dispatch(
         createDevice({ ...values, account_id: currentAccount.id })
@@ -151,7 +152,11 @@ const DeviceRegisterDialog = forwardRef((props, ref) => {
           </Input.Group>
         </Form.Item>
 
-        <Form.Item name="is_storage" valuePropName="checked">
+        <Form.Item
+          name="is_storage"
+          valuePropName="checked"
+          rules={[{ required: false }]}
+        >
           <Checkbox>Is storage</Checkbox>
         </Form.Item>
       </Form>
